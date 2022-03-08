6ce2b7cc96e87ff58d1c1deb4d1db0f57d00bf7a
Hturoy <69842773+Hturoy@users.noreply.github.com>
2022-03-08 14:29:57 +0100

    Edited link again, now it must work or i kill kill kill, please buy my nfts

--- config/env/server.js
+++ config/env/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env("https://dashboard.heroku.com/apps/group-strapi"),
+  url: env("https://dashboard.heroku.com/apps/group-assignment-noroff"),
   proxy: true,
   app: { keys: env.array("APP_KEYS", ["Key-1", "Key-2"]) },
 });
